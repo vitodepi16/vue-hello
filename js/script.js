af860a78c519f636c2a7002c51217388f4294a41
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-28 14:34:20 +0200

    make a change color on click on h1

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@ createApp({
     data(){ 
         return{
             titolo: 'Ciao ! Ti presento BARI',
-            miaclasse:'red text-center',
+            miaclasse:'red text-center ',
+            classContainer:'container d-flex justify-content-center',
             image: 'img/bari_lungomare.webp',
     }
    
 },
      methods: {
         changecolor(){
-            this.miaclasse = 'green'
+            this.miaclasse = 'green text-center'
+            
         }
  }
 }).mount('#app');
